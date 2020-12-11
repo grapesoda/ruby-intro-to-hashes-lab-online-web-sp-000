469211776e19cde1bb47c22857ce7bade0126604
Tres Tarwater <trest03@gmail.com>
2020-12-11 19:17:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -33,10 +33,21 @@ end
 
 def monopoly_with_third_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+	
+		monopoly = {
+	  :railroads => {
+	    :pieces => 4,
+	    :rent_in_dollars => {},
+	    :names => {}
+	    
+	  }
+	}
 
 end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+	
+	
 
 end
